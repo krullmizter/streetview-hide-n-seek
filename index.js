3bcee33026fcd6dfa2cb04852e60f619d2373c0d
Samuel <samuelgranvik@protonmail.com>
2023-03-02 15:58:07 +0200

    New and improved random cords. system and ability to choose difficulty

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-function randomCords() {
-    const cordinates = {
-        lat: 0,
-        lng: 0,
-    };
-
-    cordinates.lat = (Math.random() * (-90, 90) + 90).toFixed(6) * 1;
-    cordinates.lng = (Math.random() * (-180, 180) + 180).toFixed(6) * 1;
-        
-    return cordinates;
-}
-
-
-function play() {
-    difficulty = document.getElementById('difficulty').value;
-    
-    console.log(difficulty);
-}
+let map;
+let hiddenMarkers = [];
 
 function initMap() {
-  // The location of Uluru
-  initialCordinates = randomCords();
-  console.log(initialCordinates);
-  
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 0,
+      lng: 0
+    },
+    zoom: 2
   });
 }
 
 window.initMap = initMap;
+
+function play() {
+  randCords(document.getElementById('difficulty').value);
+}
+
+function randCords(difficulty) {
+  console.log('Difficulty:', difficulty);
+
+  for (let i = 0; i < difficulty; i++) {
+    const random = new google.maps.LatLng((Math.random() * (85 * 2) - 85), (Math.random() * (180 * 2) - 180));
+    const marker = new google.maps.Marker({
+      map: map,
+      position: random
+    });
+    map.setCenter(marker.getPosition());
+
+    hiddenMarkers.push(random);
+  }
+}
+
+console.log(hiddenMarkers);
